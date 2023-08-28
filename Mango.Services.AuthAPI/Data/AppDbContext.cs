24e06f8973097a7d399af3941a87d4175f380c20
Thiep Tran Trung <thiep.tran@ntq-solution.com.vn>
2023-08-28 16:42:49 +0700

    create api auth

---
+++ Mango.Services.AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Mango.Services.AuthAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.AuthAPI.Data
+{
+    /// <summary>
+    /// Information of application database context
+    /// CreatedBy: ThiepTT(28/08/2023)
+    /// </summary>
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> applicationUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Mango.Services.AuthAPI/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.AuthAPI.Models
+{
+    /// <summary>
+    /// Information of application user
+    /// CreatedBy: ThiepTT(28/08/2023)
+    /// </summary>
+    public class ApplicationUser : IdentityUser
+    {
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+    }
+}
